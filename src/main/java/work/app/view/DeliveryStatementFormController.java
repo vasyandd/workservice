f1e730f556ccdad7e52f19b66bd52baab969a2f8
Vasiliy <Kiriluk_v_r@mail.ru>
2022-03-25 02:06:06 +0300

    add validation on DeliveryStatementForm

--- src/main/java/work/app/view/DeliveryStatementFormController.java
+++ src/main/java/work/app/view/DeliveryStatementFormController.java
@@ -1,5 +1,7 @@
 package work.app.view;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.rgielen.fxweaver.core.FxmlView;
@@ -110,7 +113,7 @@ public class DeliveryStatementFormController implements Initializable {
         numberInTable.setCellValueFactory(new PropertyValueFactory<>("numberInTable"));
         productNameCol.setCellValueFactory(new PropertyValueFactory<>("productName"));
         productPriceCol.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
-        productQuantityCol.setCellValueFactory(new PropertyValueFactory<>("productQuantity"));
+        productQuantityCol.setCellValueFactory(cellData -> cellData.getValue().productQuantityProperty().asObject());
         periodCol.setCellValueFactory(new PropertyValueFactory<>("period"));
         janQuantityCol.setCellValueFactory(new PropertyValueFactory<>("janQuantity"));
         febQuantityCol.setCellValueFactory(new PropertyValueFactory<>("febQuantity"));
@@ -130,7 +133,7 @@ public class DeliveryStatementFormController implements Initializable {
         DeliveryStatement deliveryStatement = getDeliveryStatementFromTableView();
         deliveryStatementService.saveDeliveryStatement(deliveryStatement);
         numberInTableCounter = 1;
-        successSave();
+        viewSuccessSaveWindow("Ведомость поставки сохранена!");
     }
 
     private DeliveryStatement getDeliveryStatementFromTableView() {
@@ -158,16 +161,29 @@ public class DeliveryStatementFormController implements Initializable {
     }
 
     public void addRowInTable(ActionEvent event) {
-        rows.add(new DeliveryStatementRowFromTableView(numberInTableCounter++, productName.getText(),
-                Integer.parseInt(period.getText()), productPrice.getText(),
-                Integer.parseInt(janQuantity.getText()), Integer.parseInt(febQuantity.getText()),
-                Integer.parseInt(marQuantity.getText()), Integer.parseInt(aprQuantity.getText()),
-                Integer.parseInt(mayQuantity.getText()), Integer.parseInt(junQuantity.getText()),
-                Integer.parseInt(julQuantity.getText()), Integer.parseInt(augQuantity.getText()),
-                Integer.parseInt(sepQuantity.getText()), Integer.parseInt(octQuantity.getText()),
-                Integer.parseInt(novQuantity.getText()), Integer.parseInt(decQuantity.getText()))
-        );
-        clearInputFields();
+        DeliveryStatementRowFromTableView row = mapInputDataToDeliveryStatementRowFromTableView();
+        if (Objects.nonNull(row) && row.isValid()) {
+            rows.add(row);
+            clearInputFields();
+        } else {
+            viewInputDataNotValidWindow("Ввел некорректные данные, попробуй еще");
+        }
+    }
+
+
+    private DeliveryStatementRowFromTableView mapInputDataToDeliveryStatementRowFromTableView() {
+        try {
+            return new DeliveryStatementRowFromTableView(numberInTableCounter++, productName.getText(),
+                    Integer.parseInt(period.getText()), productPrice.getText(),
+                    Integer.parseInt(janQuantity.getText()), Integer.parseInt(febQuantity.getText()),
+                    Integer.parseInt(marQuantity.getText()), Integer.parseInt(aprQuantity.getText()),
+                    Integer.parseInt(mayQuantity.getText()), Integer.parseInt(junQuantity.getText()),
+                    Integer.parseInt(julQuantity.getText()), Integer.parseInt(augQuantity.getText()),
+                    Integer.parseInt(sepQuantity.getText()), Integer.parseInt(octQuantity.getText()),
+                    Integer.parseInt(novQuantity.getText()), Integer.parseInt(decQuantity.getText()));
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     private void clearInputFields() {
@@ -187,21 +203,29 @@ public class DeliveryStatementFormController implements Initializable {
         decQuantity.setText("0");
     }
 
-    private void successSave() {
+    private void viewSuccessSaveWindow(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information");
         alert.setHeaderText(null);
-        alert.setContentText("Ведомость поставки сохранена!");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void viewInputDataNotValidWindow(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Ошибка ввода");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
         alert.showAndWait();
     }
 
 
     @Getter
     @Setter
-    private static class DeliveryStatementRowFromTableView {
+    @AllArgsConstructor
+    public static class DeliveryStatementRowFromTableView {
         private int numberInTable;
         private String productName;
-        private int productQuantity;
         private int period;
         private String productPrice;
         private int janQuantity;
@@ -216,30 +240,15 @@ public class DeliveryStatementFormController implements Initializable {
         private int octQuantity;
         private int novQuantity;
         private int decQuantity;
+        
 
-        public DeliveryStatementRowFromTableView(int numberInTable, String productName, int period,
-                                                 String productPrice, int janQuantity, int febQuantity, int marQuantity,
-                                                 int aprQuantity, int mayQuantity, int junQuantity, int julQuantity,
-                                                 int augQuantity, int sepQuantity, int octQuantity, int novQuantity,
-                                                 int decQuantity) {
-            this.numberInTable = numberInTable;
-            this.productName = productName;
-            this.period = period;
-            this.productPrice = productPrice;
-            this.janQuantity = janQuantity;
-            this.febQuantity = febQuantity;
-            this.marQuantity = marQuantity;
-            this.aprQuantity = aprQuantity;
-            this.mayQuantity = mayQuantity;
-            this.junQuantity = junQuantity;
-            this.julQuantity = julQuantity;
-            this.augQuantity = augQuantity;
-            this.sepQuantity = sepQuantity;
-            this.octQuantity = octQuantity;
-            this.novQuantity = novQuantity;
-            this.decQuantity = decQuantity;
-            this.productQuantity = janQuantity + febQuantity + marQuantity + aprQuantity + mayQuantity + junQuantity
-                    + julQuantity + augQuantity + sepQuantity + octQuantity + novQuantity + decQuantity;
+        IntegerProperty productQuantityProperty() {
+            return new SimpleIntegerProperty(janQuantity + febQuantity + marQuantity + aprQuantity + mayQuantity + junQuantity
+                   + julQuantity + augQuantity + sepQuantity + octQuantity + novQuantity + decQuantity);
+        }
+
+        public boolean isValid() {
+            return true;
         }
     }
 }
